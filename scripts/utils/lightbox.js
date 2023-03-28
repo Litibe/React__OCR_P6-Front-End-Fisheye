a1c709ca1053088e6c6e3816f44b5877700dbc59
LiTiBe <contact@litiboost.fr>
2023-03-28 13:37:36 +0200

    add loading into lightbox and navig keyboard ok

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,36 +1,65 @@
 function displayLightbox() {
-  const lightbox = document.getElementById("lightbox_modal");
-  lightbox.style.display = "flex";
+  document.getElementById("lightbox_modal").style.display = "flex";
+  document.querySelector("header").style.display = "none";
+  document.querySelector("main").style.display = "none";
+  document.addEventListener("keydown", (e) => navigationKey(e));
 }
 
 function closeLightbox() {
-  const lightbox = document.getElementById("lightbox_modal");
-  lightbox.style.display = "none";
+  document.getElementById("lightbox_modal").style.display = "none";
+  document.querySelector("header").style.display = "block";
+  document.querySelector("main").style.display = "block";
+  document.removeEventListener("keydown", (e) => navigationKey(e));
 }
 
-async function updatePhotoIntoLightbox(photo) {
-  const imgBox = document.getElementsByClassName("lightbox-img")[0];
-  const titleImgBox = document.querySelector(
+async function updatePhotoIntoLightbox(media) {
+  const container = document.getElementsByClassName(
+    "lightbox-loading-container"
+  )[0];
+  // delete ancious media support
+  if (container.children[1] !== undefined) {
+    container.children[1].remove();
+  }
+  const titleMediaBox = document.querySelector(
     ".lightbox-details figure figcaption"
   );
-  imgBox.removeAttribute("src");
-  imgBox.setAttribute("src", photo.firstChild.getAttribute("src"));
-  titleImgBox.innerHTML = photo.firstChild.getAttribute("alt");
+
+  if (media.firstChild.localName === "video") {
+    mediaBox = document.createElement("video");
+    mediaBox.setAttribute("controls", "true");
+  } else if (media.firstChild.localName === "img") {
+    mediaBox = document.createElement("img");
+  }
+  for (const attr of media.firstChild.attributes) {
+    attr.name !== "class" && mediaBox.setAttribute(attr.name, attr.value);
+  }
+  container.appendChild(mediaBox);
+  titleMediaBox.innerHTML = media.firstChild.getAttribute("alt");
 
   // Search if previous and next article exist
-  if (photo.parentElement.previousElementSibling === null) {
+  if (media.parentElement.previousElementSibling === null) {
     document.getElementsByClassName("fa-chevron-left")[0].style.display =
       "none";
   } else {
     document.getElementsByClassName("fa-chevron-left")[0].style.display =
       "block";
   }
-  if (photo.parentElement.nextElementSibling === null) {
+  if (media.parentElement.nextElementSibling === null) {
     document.getElementsByClassName("fa-chevron-right")[0].style.display =
       "none";
   } else {
     document.getElementsByClassName("fa-chevron-right")[0].style.display =
       "block";
+  }
+}
+
+function navigationKey(event) {
+  if (event.key === "ArrowLeft") {
+    previousPhoto();
+  } else if (event.key === "ArrowRight") {
+    nextPhoto();
+  } else if (event.key === "Escape") {
+    closeLightbox();
   }
 }
 
@@ -50,11 +79,17 @@ function openPhoto(divPhoto) {
 }
 
 async function nextPhoto() {
-  const imgBox = document.getElementsByClassName("lightbox-img")[0];
+  const container = document.getElementsByClassName(
+    "lightbox-loading-container"
+  )[0];
+  const mediaBox = container.children[1];
   let listChild = document.getElementsByClassName("photograph__content-list")[0]
     .children;
   for (children of listChild) {
-    if (children.innerHTML.includes(imgBox.getAttribute("src")) === true) {
+    if (
+      children.innerHTML.includes(mediaBox.getAttribute("src")) === true &&
+      children.nextElementSibling !== null
+    ) {
       await updatePhotoIntoLightbox(children.nextElementSibling.firstChild);
       break;
     }
@@ -62,11 +97,17 @@ async function nextPhoto() {
 }
 
 async function previousPhoto() {
-  const imgBox = document.getElementsByClassName("lightbox-img")[0];
+  const container = document.getElementsByClassName(
+    "lightbox-loading-container"
+  )[0];
+  const mediaBox = container.children[1];
   let listChild = document.getElementsByClassName("photograph__content-list")[0]
     .children;
   for (children of listChild) {
-    if (children.innerHTML.includes(imgBox.getAttribute("src")) === true) {
+    if (
+      children.innerHTML.includes(mediaBox.getAttribute("src")) === true &&
+      children.previousElementSibling !== null
+    ) {
       await updatePhotoIntoLightbox(children.previousElementSibling.firstChild);
       break;
     }
